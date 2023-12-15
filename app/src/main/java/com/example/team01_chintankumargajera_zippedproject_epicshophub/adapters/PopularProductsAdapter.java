23c7e9115d1f97d4a9b2f3797767de7ddda6f5b2
Aizen741 <rahulsivadas2009@gmail.com>
2023-12-15 08:26:05 -0500

    Added Popular Products - Adapter

---
+++ app/src/main/java/com/example/team01_chintankumargajera_zippedproject_epicshophub/adapters/PopularProductsAdapter.java
@@ -0,0 +1,60 @@
+package com.example.team01_chintankumargajera_zippedproject_epicshophub.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.team01_chintankumargajera_zippedproject_epicshophub.R;
+import com.example.team01_chintankumargajera_zippedproject_epicshophub.models.PopularProductsModel;
+
+import java.util.List;
+
+public class PopularProductsAdapter extends RecyclerView.Adapter<PopularProductsAdapter.ViewHolder> {
+
+    private Context context;
+    private List<PopularProductsModel> list;
+
+    public PopularProductsAdapter(Context context, List<PopularProductsModel> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.popular_items, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        PopularProductsModel model = list.get(position);
+        Glide.with(context).load(model.getImg_url()).into(holder.popularImg);
+        holder.popularName.setText(model.getName());
+        holder.popularPrice.setText(String.valueOf(model.getPrice()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView popularImg;
+        TextView popularName, popularPrice;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            popularImg = itemView.findViewById(R.id.popular_img);
+            popularName = itemView.findViewById(R.id.popular_product_name);
+            popularPrice = itemView.findViewById(R.id.popular_price);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/team01_chintankumargajera_zippedproject_epicshophub/models/PopularProductsModel.java
@@ -0,0 +1,64 @@
+package com.example.team01_chintankumargajera_zippedproject_epicshophub.models;
+
+public class PopularProductsModel {
+
+    private String description;
+    private String name;
+    private String rating;
+    private int price;
+    private String img_url;
+
+    // No-argument constructor (required for Firebase)
+    public PopularProductsModel() {
+    }
+
+    // Constructor with arguments
+    public PopularProductsModel(String description, String name, String rating, int price, String img_url) {
+        this.description = description;
+        this.name = name;
+        this.rating = rating;
+        this.price = price;
+        this.img_url = img_url;
+    }
+
+    // Getters and Setters
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getImg_url() {
+        return img_url;
+    }
+
+    public void setImg_url(String img_url) {
+        this.img_url = img_url;
+    }
+}
