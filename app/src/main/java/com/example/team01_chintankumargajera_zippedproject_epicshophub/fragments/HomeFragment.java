731e367dd944626ac21b4eea615b1f1d7fad00c0
Aizen741 <rahulsivadas2009@gmail.com>
2023-12-15 05:35:30 -0500

    Added Category List

--- app/src/main/java/com/example/team01_chintankumargajera_zippedproject_epicshophub/fragments/HomeFragment.java
+++ app/src/main/java/com/example/team01_chintankumargajera_zippedproject_epicshophub/fragments/HomeFragment.java
@@ -2,7 +2,10 @@ package com.example.team01_chintankumargajera_zippedproject_epicshophub.fragment
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,24 +15,48 @@ import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.team01_chintankumargajera_zippedproject_epicshophub.R;
+import com.example.team01_chintankumargajera_zippedproject_epicshophub.adapters.CategoryAdapter;
+import com.example.team01_chintankumargajera_zippedproject_epicshophub.models.CategoryModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
+
 public class HomeFragment extends Fragment {
 
-    public HomeFragment() {
-        // Required empty public constructor
-    }
 
 
+    // Category RecyclerView
+
+    RecyclerView catRecyclerview;
+    CategoryAdapter categoryAdapter;
+    List<CategoryModel>categoryModelList;
+
+
+
+    //FireStore
+    FirebaseFirestore db;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
         View root = inflater.inflate(R.layout.fragment_home, container , false);
+        catRecyclerview = root.findViewById(R.id.rec_category);
+
+
+
+        db = FirebaseFirestore.getInstance();
+
+
+
 
         // Image slider
         ImageSlider imageSlider = root.findViewById(R.id.image_slider);
@@ -40,6 +67,42 @@ public class HomeFragment extends Fragment {
         slideModels.add(new SlideModel(R.drawable.banner3, "70% OFF", ScaleTypes.CENTER_CROP));
 
         imageSlider.setImageList(slideModels);
+
+
+    // Category
+        catRecyclerview.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL,false));
+
+        categoryModelList = new ArrayList<>();
+        categoryAdapter = new CategoryAdapter(getContext(),categoryModelList);
+        catRecyclerview.setAdapter(categoryAdapter);
+
+
+
+        db.collection("Category")
+                .get()
+
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+
+
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            for (QueryDocumentSnapshot document : task.getResult()){
+                                CategoryModel categoryModel = document.toObject(CategoryModel.class);
+                                categoryModelList.add(categoryModel);
+                                categoryAdapter.notifyDataSetChanged();
+                            }
+                        }else{
+
+                        }
+                    }
+                });
+
+
+
+
+
+
 
         return root;
     }
