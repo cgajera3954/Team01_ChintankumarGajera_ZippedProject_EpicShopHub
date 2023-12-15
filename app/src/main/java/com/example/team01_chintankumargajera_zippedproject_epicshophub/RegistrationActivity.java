72f8a4c995691011cfb0a1d046a9d975a2edd6b5
Rutuja Mundhe <rutujamundhe15@gmail.com>
2023-12-14 21:59:16 -0500

    Log In and Sign Up page created

---
+++ app/src/main/java/com/example/team01_chintankumargajera_zippedproject_epicshophub/RegistrationActivity.java
@@ -0,0 +1,27 @@
+package com.example.team01_chintankumargajera_zippedproject_epicshophub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+
+    public void signup(View view)
+    {
+        startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+    }
+
+    public void signin(View view)
+    {
+        startActivity(new Intent(RegistrationActivity.this,LoginActivity.class));
+    }
+
+}
